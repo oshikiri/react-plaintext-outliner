18e2c7c9f3161fc44acf2ffcda0ba0aa84dc17a4
Takamasa Oshikiri <mail@oshikiri.org>
2025-04-05 20:18:50 +0900

    Move DOM utility functions to a new module

--- src/BlockComponent.jsx
+++ src/BlockComponent.jsx
@@ -3,6 +3,7 @@ import { useRef, useEffect } from "react";
 import { useStore } from "./state";
 import BlockEntity from "./BlockEntity";
 import { indent, outdent } from "./edit";
+import { getOffset, setCursor, getTextsAroundCursor } from "./dom";
 
 export default function BlockComponent({ block }) {
   const cursorPosition = useStore((state) => state.cursorPosition);
@@ -123,33 +124,6 @@ function createNext(block, beforeCursor, afterCursor) {
   const [_parent, idx] = block.getParentAndIdx();
   block.parent.children.splice(idx + 1, 0, newBlock);
   return { block, newBlock };
-}
-
-function getTextsAroundCursor() {
-  const selection = window.getSelection();
-  const range = selection.getRangeAt(0);
-  const text = range.startContainer.textContent;
-  const beforeCursor = text.substring(0, range.startOffset);
-  const afterCursor = text.substring(range.endOffset);
-  return { beforeCursor, afterCursor, startOffset: range.startOffset };
-}
-
-function getOffset(node, startOffset) {
-  const nextInnerText = node.innerText || "";
-  if (startOffset >= nextInnerText.length) {
-    return nextInnerText.length;
-  }
-  return startOffset;
-}
-
-function setCursor(node, offset) {
-  const range = document.createRange();
-  range.setStart(node, offset);
-  range.setEnd(node, offset);
-
-  const selection = window.getSelection();
-  selection.removeAllRanges();
-  selection.addRange(range);
 }
 
 export { createNext };
